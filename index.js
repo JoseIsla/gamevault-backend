c5607bed6ae683df0cee22865628023b7d42a98a
José Isla Pérez <islaperezjose@gmail.com>
2025-05-22 11:52:47 +0200

    Correccion alertas de Render

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ app.get('/', (req, res) => {
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+
 })
   .then(() => console.log("✅ Conectado a MongoDB Atlas"))
   .catch(err => {
